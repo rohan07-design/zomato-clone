e31b58d697bdc1704627ecff2fd7a29d3c174196
rohan007 <rohanparande007@gmail.com>
2022-10-02 16:10:01 +0530

    setup the server.js

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const expressLayout = require('express-ejs-layouts');
+const path = require('path');
+const PORT = process.env.PORT || 3000
+
+app.get("/",(req,res) => {
+    res.render("home.ejs")
+})
+
+//setting the template engine
+app.use(expressLayout)
+app.set("views", path.join(__dirname,"/resources/views"))
+app.set("view engine","ejs");
+
+app.listen(PORT, () => {
+    console.log(`Listening at ${PORT}`);
+})
