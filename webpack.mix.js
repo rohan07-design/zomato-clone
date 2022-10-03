3014324d0f49bc53260cb48fc0e8fcfa213d93a8
rohan007 <rohanparande007@gmail.com>
2022-10-03 16:26:32 +0530

    Added navbar and brand name

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,4 +1,4 @@
 let mix = require('laravel-mix');
 
 
-mix.js('resources/js/app.js','public/js/app.js').css('resources/css/app.css','public/css/app.css')
+mix.js('resources/js/app.js','public/js/app.js').postCss('resources/css/app.css','public/css/app.css')
